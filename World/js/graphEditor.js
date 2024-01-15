7a13fa1c0d68c483f8bab62f9ecf223541bebb5e
Sondre Garnæs <sondre433@gmail.com>
2024-01-15 16:34:51 +0100

    continued on the graph editor

--- World/js/graphEditor.js
+++ World/js/graphEditor.js
@@ -3,6 +3,54 @@ class GraphEditor {
         this.canvas = canvas;
         this.graph = graph;
 
-        this.ctx = this.canvas.getContext("2d")
+        this.ctx = this.canvas.getContext("2d");
+
+        this.selected = null;
+        this.hovered = null;
+
+        this.#addEventListeners();
     }
-}
+
+    #addEventListeners() {
+
+        this.canvas.addEventListener("mousedown", (evt) => {
+            if (evt.button == 2) { //right click
+                if(this.hovered) {
+                    this.#removePoint(this.hovered);
+                }
+            }
+            const mouse = new Point(evt.offsetX, evt.offsetY);
+            if (this.hovered) {
+                this.selected = this.hovered;
+                return;
+            }
+            this.graph.addPoint(mouse);
+            this.selected=mouse;
+        });
+        this.canvas.addEventListener("mousemove", (evt) => {
+            const mouse = new Point(evt.offsetX, evt.offsetY);
+            this.hovered = getNearestPoint(mouse, this.graph.points,50);
+        });
+        this.canvas.addEventListener("contextmenu", (evt) => evt.preventDefault());
+    }
+
+    #removePoint(point) {
+        this.graph.removePoint(point);
+        this.hovered = null;
+        if (this.selected == point){
+            this.selected = null;
+        }
+        
+    }
+
+    display() {
+        this.graph.draw(this.ctx)
+        if (this.hovered) {
+            this.hovered.draw(this.ctx, { fill: true});
+        }
+        if (this.selected) {
+            this.selected.draw(this.ctx, { outline: true});
+        }
+        
+    }
+}  
