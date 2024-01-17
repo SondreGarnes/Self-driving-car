e12debf8f3ce1bc61ad74f123426fbaef61bf63d
Sondre Garnæs <sondre433@gmail.com>
2024-01-17 15:16:02 +0100

    Finished the dynamic viewport

--- World/js/graphEditor.js
+++ World/js/graphEditor.js
@@ -16,7 +16,6 @@ class GraphEditor {
 
     #addEventListeners() {
         this.canvas.addEventListener("mousedown", this.#handleMouseDown.bind(this));
-            
         this.canvas.addEventListener("mousemove", this.#handleMouseMove.bind(this));
         this.canvas.addEventListener("contextmenu", (evt) => evt.preventDefault());
         this.canvas.addEventListener("mouseup", () => this.dragging=false);
@@ -68,6 +67,12 @@ class GraphEditor {
             this.selected = null;
         }
         
+    }
+
+    dispose() {
+        this.graph.dispose();
+        this.selected=null;
+        this.hovered=null;
     }
 
     display() {
